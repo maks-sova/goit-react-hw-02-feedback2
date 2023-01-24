25ace807a54a7b8007797a42a9dd16181df8c116
maks-sova <maxxxhill2k@gmail.com>
2023-01-25 01:29:01 +0200

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import Section from './Section/Section';
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      <Section title="Привет Макс " />
+    </div>
+  );
+};
---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import css from './Section.css';
+
+export default function Section({ title, children }) {
+  return <h2>{title}</h2>;
+}
